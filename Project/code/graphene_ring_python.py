e2ded418bd0ab8cf60b78238e819aa48c4e81517
Firerom <romain63@hotmail.com>
2018-12-06 01:51:32 +0100

    potential work

--- Project/code/graphene_ring_python.py
+++ Project/code/graphene_ring_python.py
@@ -12,7 +12,7 @@ import math
 from math import atan2, pi, sqrt, cos, sin
 from cmath import exp
 from types import SimpleNamespace
-
+from collections import namedtuple
 #%% [markdown]
 # #### Fundamental constant 
 
@@ -23,7 +23,8 @@ c = 299792458       # Light celerity (m/s)
 kB = 1.3806503e-23  # Boltzmann constant (kg m2/Ks2)
 m_e = 9.1e-31       # electron mass (kg)
 a = 1 # new constant
-
+param_syst = namedtuple("param_syst", "a t R_ext W W_L L_L magn_activated potential_activated")
+param_pot=namedtuple("param_pot", "theta_min theta_max choice_VG VG")
 #%% [markdown]
 # #### Definition of the graphene function with some parameters
 # 
@@ -34,27 +35,93 @@ a = 1 # new constant
 # 
 
 #%%
-def make_system(a=1.0,t=1.0,R_ext=350,W=150,W_L=150,L_L=100,magn_activated=0, potential_activated=0):
-    
+
+
+# definition of the shape of the potential
+def potential_VG(pos,param_pot):
+        #x, y = pos
+        ## work in  polar coordinate since it is a ring
+        #r = sqrt(x**2 + y**2)
+        #theta = atan2(y,x)
+        
+         ##function Vg
+        
+        # constant 
+        if param_pot.choice_VG=='constant':
+            VG=param_pot.VG
+            return VG
+            
+        # linear 
+        elif param_pot.choice_VG=='linear':
+            VG=0
+            print('still not done, choose 1 ')
+            return VG
+            
+            
+        # quadratic
+        elif param_pot.choice_VG=='quadratic':
+            VG=0
+            print('still not done, choose 1 ')
+            return VG
+        else: 
+            return 0
+# definition of the potential. Take the position of the site, check in which arm we 
+    # are. then depending on the arm we are, the function calls another function that 
+    # computes the potential depending on the choice of the form 
+def potential(site,param_pot_1,param_pot_2):
+
+        x, y = site.pos
+        ## work in  polar coordinate since it is a ring
+        #r = sqrt(x**2 + y**2) # in the first place we dont considered the position along the radial direction 
+        # since we assume a constant potential in the width of the arm
+        theta = atan2(y,x)
+        if 0<y :
+            if   param_pot_1.theta_min <theta< param_pot_1.theta_max:
+                return  potential_VG(site.pos,param_pot_1)
+            else: 
+                return 0
+        elif 0>y: 
+            if   param_pot_2.theta_min <theta< param_pot_2.theta_max:
+                return  potential_VG(site.pos,param_pot_2)
+            else: 
+                return 0
+        else:
+            return 0
+def make_system(param, param_pot_1,param_pot_2):#a=1.0,t=1.0,R_ext=350,W=150,W_L=150,L_L=100,magn_activated=0, potential_activated=0):
+    a=param.a
+    t=param.t
+    R_ext=param.R_ext
+    W=param.W
+    W_L=param.W_L
+    L_L=param.L_L
+    magn_activated=param.magn_activated
+    potential_activated=param.potential_activated
+
     graphene = kwant.lattice.honeycomb(a,norbs=1)
     a_lat, b_lat = graphene.sublattices
     sys = kwant.Builder()
- 
     # definition geometry
     def circle(pos): return (pos[0]**2 + pos[1]**2 <R_ext**2  and pos[0]**2 + pos[1]**2 >(R_ext-W)**2)                             or ((R_ext-W/2)<pos[0]<(R_ext+L_L )
                             and abs(pos[1])<W_L/2) \
                             or (-(R_ext-W/2)>pos[0]>-(R_ext+L_L) 
                             and abs(pos[1])<W_L/2) 
+    # onsite energy
+    if potential_activated==0:
+       sys[graphene.shape(circle, (R_ext-W/2,0))] = 0 
+    elif potential_activated==1:
+        sys[graphene.shape(circle, (R_ext-W/2,0))]=potential
+    else:
+         print('Wrong input parameter for the potential')
+    
     # definition hopping for magentic field        
-    def hopping(site_i, site_j, phi):
+    def hopping(site_i, site_j, phi,param_pot_1,param_pot_2):
         xi, yi = site_i.pos
         xj, yj = site_j.pos
         return -t * exp(-0.5j*phi/(h/e)/pi*(xi-xj)*(yi+yj))
 
-    sys[graphene.shape(circle, (R_ext-W/2,0))] = 0 # onsite energy is zero (later a potential will be applied)
     hoppings = (((0, 0), a_lat, b_lat), ((0, 1), a_lat, b_lat), ((-1, 1), a_lat, b_lat))
     
-     
+    # hopping term
     if magn_activated==0:
          sys[[kwant.builder.HoppingKind(*hoppingg) for hoppingg in hoppings]] = -t
     
@@ -64,21 +131,20 @@ def make_system(a=1.0,t=1.0,R_ext=350,W=150,W_L=150,L_L=100,magn_activated=0, po
     else:
         print('Wrong input parameter for the magnetic field')
    
+
     
     
-    
-    
-    
+    # lead 
     sym=kwant.TranslationalSymmetry(graphene.vec((1,0)))
     def lead_shape(R): return abs(R[1]) < W_L/2
     
     Hlead =kwant.Builder(sym)
-    if potential_activated==0:
-        Hlead[graphene.shape(lead_shape,(0,0) )]=0
-    elif potential_activated==1:
-        Hlead[graphene.shape(lead_shape,(0,0) )]=potential(site,choice_VG_1,choice_VG_2,Vg_value_1,Vg_value_2 )
-    else
-         print('Wrong input parameter for the potential')
+    #if potential_activated==0:
+    Hlead[graphene.shape(lead_shape,(0,0) )]=0
+    #elif potential_activated==1:
+    #    Hlead[graphene.shape(lead_shape,(0,0) )]=potential
+    #else:
+    # print('Wrong input parameter for the potential')
     
     if magn_activated==0:
          Hlead[graphene.neighbors()]=-t
@@ -97,84 +163,37 @@ def make_system(a=1.0,t=1.0,R_ext=350,W=150,W_L=150,L_L=100,magn_activated=0, po
     
     return sys
 
-#%% [markdown]
-# #### Definition of the potential
-# 
 
-#%%
-def potential(site,choice_VG_1,choice_VG_2,Vg_value_1,Vg_value_2 ):
-
-    x, y = site.pos
-    ## work in  polar coordinate since it is a ring
-    r = sqrt(x**2 + y**2)
-    theta = atan2(y,x)
-    
-    if  theta_1 <theta< theta_2:
-        choice_VG=choice_VG_1
-        Vg_value=Vg_value_1
-        return  potential_VG(pos,choice_VG,Vg_value)
-    elif -theta_2 <theta< -theta_1:
-        choice_VG=choice_VG_2
-        Vg_value=Vg_value_2
-        return  potential_VG(pos,choice_VG,Vg_value)
-        
-    else
-        return 0
-     
-    
-    
    
 
 
-#%%
-def potential_VG(pos, choice_VG,Vg_value):
-#def potential_VG(pos, choice_VG,choiceVG,Vg_value): 
-    x, y = site.pos
-    ## work in  polar coordinate since it is a ring
-    r = sqrt(x**2 + y**2)
-    theta = atan2(y,x)
-    
-    ##function Vg
-    
-    # constant is 1
-    if choice_VG=='constant':
-        VG=Vg_value
-        
-        
-    # linear is 2 
-    if choice_VG=='linear':
-        VG=0
-        print('still not done, choose 1 ')
-
-        
-        
-    # quadratic
-    if choice_VG=='quadratic':
-        VG=0
-        print('still not done, choose 1 ')
-
-    
-    return VG
 
 #%% [markdown]
 # #### Creation of the physical graphene ring
 
 #%%
-scaling_fact=20
 #n2D=1.2e16
 #scaling_fact=25
+scaling_fact=20
 a0=0.246
 a=a0*scaling_fact
 t0=2.8
 t=t0/scaling_fact
-R_ext=350
-W=100
-W_L=150
-L_L=100
-pot = 0.1
-magn_activated=0
-H=make_system(a,t,R_ext,W,W_L,L_L,magn_activated, pot)
+
+
+param = param_syst(a=a0*scaling_fact, t=t,R_ext=350, W=100,W_L=150,L_L=100,magn_activated=0,potential_activated=0)
+
+
+param_pot_1=param_pot(theta_min=45/180*pi,theta_max=135/180*pi,choice_VG='constant',VG=2*t)
+param_pot_2=param_pot(theta_min=-70/180*pi,theta_max=-45/180*pi,choice_VG='constant',VG=1*t)
+
+
+H=make_system(param,param_pot_1,param_pot_2)
 kwant.plot(H)
+
+Hf=H.finalized()
+vals=[potential(Hf.sites[n], param_pot_1,param_pot_2) for n in range(Hf.graph.num_nodes)]
+kwant.plotter.map(Hf, vals)
 
 #%% [markdown]
 # ## Analysis of the transmission
@@ -187,16 +206,16 @@ a0=0.246
 a=a0*scaling_fact
 t0=2.8
 t=t0/scaling_fact
-R_ext=350
-W=200
-W_L=150
-L_L=100
-magn_activated=0
-
 
 #%%
 get_ipython().magic('matplotlib inline')
-H=make_system(a,t,R_ext,W,W_L,L_L,magn_activated)
+#H=make_system(a,t,R_ext,W,W_L,L_L,magn_activated)
+param = param_syst(a=a0*scaling_fact, t=t,R_ext=350, W=100,W_L=150,L_L=100,magn_activated=0,potential_activated=0)
+
+param_pot_1=param_pot(theta_min=45/180*pi,theta_max=135/180*pi,choice_VG='constant',VG=2*t)
+param_pot_2=param_pot(theta_min=-135/180*pi,theta_max=-45/180*pi,choice_VG='constant',VG=0)
+
+H=make_system(param,param_pot_1,param_pot_2)
 
 Hf=H.finalized()
 #max_E=0.1
@@ -221,15 +240,21 @@ Hf=H.finalized()
 #plt.xlabel('Energy E')
 #plt.show()
 
+#smatrix = kwant.smatrix(Hf, energy = 0.13,args=[param_pot_1, param_pot_2])
+#T = smatrix.transmission(1,0)
+#print(T)
+#    T2.append(T)
+#    #print(x)
 
-E = np.linspace(-0.13,0.13,500)
+
+E = np.linspace(-0.13,0.13,10)
 #print(E)
 T2 = []
 for x in E:
-    smatrix = kwant.smatrix(Hf, energy = x)
+    smatrix = kwant.smatrix(Hf, energy = x,args=[param_pot_1, param_pot_2])
     T = smatrix.transmission(1,0)
     T2.append(T)
-    #print(x)
+#    #print(x)
     
 #print(T2)    
 plt.plot(E,T2)    
@@ -248,22 +273,21 @@ scaling_fact=20
 #scaling_fact=25
 a0=0.246
 a=a0*scaling_fact
-#e=1.602e-19
 t0=2.8
 t=t0/scaling_fact
-R_ext=350
-W=100
-W_L=150
-L_L=100
-magn_activated=1 # we activate the magnetic field here !
 
 
 
 #%%
 
+param = param_syst(a=a0*scaling_fact, t=t,R_ext=350, W=100,W_L=150,L_L=100,magn_activated=1,potential_activated=0)
+
+param_pot_1=param_pot(theta_min=45/180*pi,theta_max=135/180*pi,choice_VG='constant',VG=2*t)
+param_pot_2=param_pot(theta_min=-135/180*pi,theta_max=-45/180*pi,choice_VG='constant',VG=0)
 
 
-H=make_system(a,t,R_ext,W,W_L,L_L,magn_activated)
+
+H=make_system(param,param_pot_1,param_pot_2)
 
 H_mf=H.finalized()
 
@@ -281,7 +305,7 @@ G = np.zeros([N,1])
 
 for i,B in enumerate(Bs):
     phi = B * a**2 *sqrt(3)/2 * (1e-18)
-    smatrix = kwant.smatrix(H_mf, energy = E,args=[phi] ) # transmission matrix (here this)
+    smatrix = kwant.smatrix(H_mf, energy = E,args=[phi,param_pot_1,param_pot_2] ) # transmission matrix (here this)
     T = smatrix.transmission(1, 0) # transmission value obtained from the left lead towards the right lead
     G[i] = T
     
@@ -304,4 +328,7 @@ plt.show()
 
 
 
-
+#%%
+MyStruct = namedtuple("MyStruct", "field1 field2 field3")
+m = MyStruct("foo",1, "baz")
+m.field2
